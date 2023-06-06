f8dc31ac5160247a9b079eb64c44f157c3b5b0ae
Lucas Mercado <lucasmmercado99@gmail.com>
2023-06-06 19:13:12 -0300

    Funciones JS, Primera Entrega JS

---
+++ JS/app.js
@@ -0,0 +1,58 @@
+console.log("Esta conectado");
+let porcentaje = " ";
+
+
+function validarEdad() {
+    let edad = 0;
+    while (edad < 18) {
+        edad = prompt("Hola inserte su edad: ");
+        if (edad >= 18) {
+            break
+        } else {
+            alert("Para acceder a este sitio web debe ser mayor de edad. Puede volver en " + (18 - edad) +
+                " año/s, o llamar a un mayor.");
+        }
+    }
+    alert("Es mayor de edad, puede ingresar.");
+}
+
+function codigoDescuento() {
+    let tieneCodigoDesc = prompt("¿Posee código de descuento? (Inserte S para sí)");
+    if (tieneCodigoDesc === "S" | tieneCodigoDesc === "s") {
+        porcentaje = prompt("Inserte código promocional: ");
+        switch (porcentaje) {
+            case "CINCOOFF":
+                alert("Usted tendrá un 5% de descuento en todo el sitio.");
+                console.log("Se le aplicara un 5% OFF a todos los precios que se visualizan en el sitio.");
+                console.log("Además si abona en efectivo o transferencia se aplica un 10% OFF acumulable.")
+                break;
+            case "DIEZOFF":
+                alert("Usted tendrá un 10% de descuento en todo el sitio.");
+                console.log("Se le aplicara un 10% OFF a todos los precios que se visualizan en el sitio.");
+                console.log("Además si abona en efectivo o transferencia se aplica un 10% OFF acumulable.")
+                break;
+            case "VEINTEOFF":
+                alert("Usted tendrá un 20% de descuento en todo el sitio.");
+                console.log("Se le aplicara un 20% OFF a todos los precios que se visualizan en el sitio.");
+                console.log("Además si abona en efectivo o transferencia se aplica un 10% OFF acumulable.")
+                break;
+            default:
+                alert("No ha ingresado un código válido.");
+        }
+    }
+
+}
+
+validarEdad();
+
+codigoDescuento();
+
+if (porcentaje == " ") {
+    console.log("Usted puede conseguir código de descuentos en nuestras empresas asociadas: ");
+    for (let i = 0; i <= 6; i += 1) {
+        console.log(i + " Empresa x");
+        //Esto nombraría las empresas asociadas recorriendo un array que las contenga con un lenght de condición
+    }
+    console.log("Igualmente, si abona en efectivo o transferencia se aplica un 10% OFF.")
+}
+
